dc9705765ade15d57535f6f520319b388cb7da8f
norbcodes <thenorbert2008@gmail.com>
2023-02-17 23:00:53 +0100

    Added basic score system.

--- PongEngine.py
+++ PongEngine.py
@@ -3,7 +3,7 @@
 # Import the goodies!
 from clsCmd import clear
 from math import radians, degrees
-from random import randint
+from random import randint, choice
 from time import sleep
 import keyboard
 
@@ -16,9 +16,9 @@ def LoadText():
     global TITLE
     txt = open('PongTitles.txt', 'rt', encoding='UTF-8')
     for _ in range(8): TITLE += txt.readline()
-    for _ in range(11):
+    for _ in range(10):
         temp = ''
-        for z in range(8): temp += txt.readline()
+        for z in range(5): temp += txt.readline().removesuffix('\n')
         NUMBERS.append(temp)
 
 class World:
@@ -30,6 +30,9 @@ class World:
     Ball_Vel = [None, None]
     MapW = 81
     MapH = 21
+    Score = [None, None]
+    BallRespawnTime = FPS * 2  # 5 seconds.
+    BallRespawnTimer = None
 
     StartPrompt = None
     Paused = None
@@ -42,6 +45,8 @@ class World:
         self.Ball_Vel = [1, 2]
         self.StartPrompt = True
         self.Paused = True
+        self.Score = [0, 0]
+        self.BallRespawnTimer = 0
 
     def Render(self):
         clear()
@@ -66,13 +71,28 @@ class World:
             LineY += 1
 
         # Render ball
-        output[self.Ball_X + (self.Ball_Y * self.MapW)] = '0'
+        if self.BallRespawnTimer == 0:
+            output[self.Ball_X + (self.Ball_Y * self.MapW)] = '0'
 
         if self.StartPrompt:
             text = "Press Enter to play. Press ESC to quit at any time. Press P to pause."
             x, y = (self.MapW - len(text))//2, 3
             for char in range(len(text)):
                 output[(x + char) + (y * self.MapW)] = text[char]
+
+        # Render score, if Start Prompt is not shown.
+        if not self.StartPrompt:
+            score1 = NUMBERS[self.Score[0]]
+            x, y = self.MapW//2 - 7, 1
+            for A in range(5):
+                for B in range(5):
+                    output[x+B + ((y+A) * self.MapW)] = score1[B + (A * 5)]
+
+            score2 = NUMBERS[self.Score[1]]
+            x, y = self.MapW // 2 + 3, 1
+            for A in range(5):
+                for B in range(5):
+                    output[x + B + ((y + A) * self.MapW)] = score2[B + (A * 5)]
 
         indx = 0
         for x in output:
@@ -87,18 +107,34 @@ class World:
         print(' ' + '-' * self.MapW)
 
     def AdvanceBall(self):
+        if self.BallRespawnTimer != 0:
+             return
+
         if self.Ball_Y > self.MapH-2 or self.Ball_Y < 1:
             self.Ball_Vel[0] *= -1
 
         if self.Ball_X > self.MapW-2 or self.Ball_X < 2:
-            self.Ball_Vel[1] *= -1
+            if self.Ball_X < self.MapW//2:
+                self.Score[1] += 1
+                self.BallRespawnTimer = self.BallRespawnTime
+                self.Ball_Y, self.Ball_X = self.MapH // 2, self.MapW // 2
+                self.Ball_Vel[0] *= choice((-1, 1))
+                self.Ball_Vel[1] *= choice((-1, 1))
+                return
+
+            self.Score[0] += 1
+            self.BallRespawnTimer = self.BallRespawnTime
+            self.Ball_Y, self.Ball_X = self.MapH // 2, self.MapW // 2
+            self.Ball_Vel[0] *= choice((-1, 1))
+            self.Ball_Vel[1] *= choice((-1, 1))
+            return
 
         if self.Ball_X not in range(12, self.MapW - 12):
             if self.Ball_X > self.MapW//2:
-                if self.Ball_X > (self.MapW - self.Pad_x - 4) and self.Ball_Y in range(self.P2_pad_y-2, self.P2_pad_y+2):
+                if self.Ball_X > (self.MapW - self.Pad_x - 4) and self.Ball_Y in range(self.P2_pad_y-3, self.P2_pad_y+3):
                     self.Ball_Vel[1] *= -1
             else:
-                if self.Ball_X < self.Pad_x + 4 and self.Ball_Y in range(self.P1_pad_y-2, self.P1_pad_y+2):
+                if self.Ball_X < self.Pad_x + 4 and self.Ball_Y in range(self.P1_pad_y-3, self.P1_pad_y+3):
                     self.Ball_Vel[1] *= -1
 
         self.Ball_Y += self.Ball_Vel[0]
@@ -146,14 +182,18 @@ if __name__ == '__main__':
         if keyboard.is_pressed('DOWN'):
             KEYS[3] = True
 
-        if KEYS[0]:
-            World.MovePad(True, False)
-        if KEYS[1]:
-            World.MovePad(True, True)
-        if KEYS[2]:
-            World.MovePad(False, False)
-        if KEYS[3]:
-            World.MovePad(False, True)
+        if World.BallRespawnTimer == 0:
+            if KEYS[0]:
+                World.MovePad(True, False)
+            if KEYS[1]:
+                World.MovePad(True, True)
+            if KEYS[2]:
+                World.MovePad(False, False)
+            if KEYS[3]:
+                World.MovePad(False, True)
+
+        if World.BallRespawnTimer != 0:
+            World.BallRespawnTimer -= 1
 
         World.Render()
         World.AdvanceBall()
