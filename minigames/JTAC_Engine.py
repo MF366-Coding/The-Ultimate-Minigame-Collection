2faf0842f5e58e591a1d89be02c712ac3ea76504
norbcodes <thenorbert2008@gmail.com>
2024-02-12 20:48:22 +0100

    Nothing much... yet... >.<

--- minigames/JTAC_Engine.py
+++ minigames/JTAC_Engine.py
@@ -7,12 +7,16 @@
 
 import os
 import sys
+import time
 
 clear = None
 
 # Constants
-DISTANCE: int = 40174991951811150
+_ALPHA_CENTAURI: int = 40_174_991_951_811_150  # meters
 TITLE: str = ""
+FPS: int = 60
+# Engines (values not accurate to real life)
+BaseEngine: tuple[int, int] = (2, 0.2)
 # yay
 
 def LoadTitle():
@@ -20,13 +24,53 @@ def LoadTitle():
     with open( os.path.join(os.path.dirname(__file__), 'JTAC_Titles.txt'), "rt", encoding="UTF-8" ) as File:
         for _ in range(8): TITLE += File.readline()
 
+class Engine:
+    Power: int = None  # Acceleration per second
+    Consumption: int = None  # Fuel consumption per second
+
+    def __init__(self, eng_stats: tuple[int, int]):
+        self.Power, self.Consumption = eng_stats[0], eng_stats[1]
+
+class PlayerClass:
+    X: int = None
+    Fuel: int = None
+    MaxFuel: int = None
+    CurrentUpgrades: dict = None
+
+    def __init__(self, load_from: str = None):
+        global BaseEngine
+        # INIT BITCHHHHH
+        self.X = 0  # On Earth!
+        self.Fuel, self.MaxFuel = 100, 100  # Base fuel
+        self.CurrentUpgrades = {
+            "Engine": Engine(BaseEngine)
+        }
+
+class WorldClass:
+    Player: PlayerClass = None
+    Distance: int = None
+
+    def __init__(self):
+        global _ALPHA_CENTAURI
+        # INIT!!!!
+        self.Player = PlayerClass()
+        self.Distance = _ALPHA_CENTAURI
+
+def JTAC():
+    global clear, FPS
+    World: WorldClass = WorldClass()
+
+    while True:
+        time.sleep(1/FPS)
+
 def RunGame(_tumc_globs: dict):
     global TITLE, clear
-
     # Yep
     if clear is None: clear = _tumc_globs["s"].clear
-
+    # Load shit
     LoadTitle()
+    # Play!
+    JTAC()
 
 if __name__ == '__main__':
     # THIS RUNS ONLY WHEN THIS FILE IS RAN AS A STANDALONE SCRIPT
